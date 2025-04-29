b42e31281b776bf2a0f680d2aa9467d6a8421b88
Savannah Boyer <199019412+sboyer96@users.noreply.github.com>
2025-04-29 13:33:22 -0400

    Initial Commit

---
+++ Cookbook/CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookApp.swift
+//  Cookbook
+//
+//  Created by Savannah on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
